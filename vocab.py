4a202a0a1fa60b4dc3f72818e80b1772df1e9ddd
Pumpkin-spice-cloud <alan_mo@live.com>
2020-06-12 12:09:31 -0400

    modify comment str selection
    - Comments are now capped at slightly over 20000 tokens
    - Doc length is checked every 20 comments
    - Now pull comments from top posts of the year
    - Add period to end of comments if it does not have one to better
    sentence length calculation

--- vocab.py
+++ vocab.py
@@ -4,6 +4,7 @@ import json
 import time
 import spacy
 import os
+import timeit
 nlp = spacy.load('en_core_web_sm')
 reddit = praw.Reddit('personal')
 # Change subreddits to whatever ones you want to analyze
@@ -25,16 +26,23 @@ class subreddit_analysis:
     def fetch_subreddit_comments(self):
         subreddit_inst = reddit.subreddit(self.subreddit)
         comment_str = ''
-        for i, submission in enumerate(subreddit_inst.top('all', limit=10)):
+        for i, submission in enumerate(subreddit_inst.top('year')):
             top_level_comments = list(submission.comments)
-            for comment in top_level_comments:
+            for index, comment in enumerate(top_level_comments):
                 # do not concat if comment says More Comments
                 if isinstance(comment, praw.models.MoreComments):
                     continue
                 # do not concat if comment removed
                 if (comment.body != '[removed]'):
                     comment_str = comment_str + comment.body
-        return comment_str
+                    # add period if comment does not end with one
+                    if (comment.body[-1] != '.'):
+                        comment_str = comment_str + '.'
+                if (index % 20 == 0):
+                    # check doc length every 20 comments
+                    if (len(subreddit_analysis.nlp(comment_str)) > 20000):
+                        print(len(subreddit_analysis.nlp(comment_str)))
+                        return comment_str
 
     @property
     def avg_sentence_length(self):
@@ -55,11 +63,14 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 fileName = timestr + '-data.txt'
 file_path = os.path.join(output_dir, fileName)
 for subreddit in subreddits:
+    start = timeit.default_timer()
     result = subreddit_analysis(subreddit)
     output_json[subreddit] = {
         "mtld": result.mtld,
         "avg_sentence_length": result.avg_sentence_length
         }
+    stop = timeit.default_timer()
+    print(subreddit, ' took ', stop - start)
 # Generate data subdirrector if it does not exist
 try:
     os.mkdir(output_dir)
